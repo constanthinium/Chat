cdf49581da95b8ea96c6acea32fbf7b5b3fcae9a
constanthinium <constanthinium@gmail.com>
2021-01-20 09:58:26 +0300

    Remove global exception handling

--- Chat/App.xaml.cs
+++ Chat/App.xaml.cs
@@ -1,14 +1,9 @@
 ﻿using System.Windows;
-using System.Windows.Threading;
 
 namespace Chat
 {
     public partial class App : Application
     {
-        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
-        {
-            MessageBox.Show("Error: " + e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            e.Handled = true;
-        }
+
     }
 }
